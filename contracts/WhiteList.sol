b0f3836c480f042ce54c02f6a5bf79b1d9db6763
freeLTC <freemanjackal@gmail.com>
2018-02-19 13:06:26 -0800

    whitelist

---
+++ contracts/WhiteList.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.2;
+
+import "./Owned.sol";
+
+contract WhiteList is Owned {
+  mapping (address => bool) public investorWhiteList;
+
+  mapping (address => address) public referralList;
+
+  function InvestorWhiteList() {
+
+  }
+
+  function addInvestorToWhiteList(address investor) external onlyOwner {
+    require(investor != 0x0 && !investorWhiteList[investor]);
+    investorWhiteList[investor] = true;
+  }
+
+  function removeInvestorFromWhiteList(address investor) external onlyOwner {
+    require(investor != 0x0 && investorWhiteList[investor]);
+    investorWhiteList[investor] = false;
+  }
+
+  //when new user will contribute ICO contract will automatically send bonus to referral
+  function addReferralOf(address investor, address referral) external onlyOwner {
+    require(investor != 0x0 && referral != 0x0 && referralList[investor] == 0x0 && investor != referral);
+    referralList[investor] = referral;
+  }
+
+  function isAllowed(address investor) constant external returns (bool result) {
+    return investorWhiteList[investor];
+  }
+
+  function getReferralOf(address investor) constant external returns (address result) {
+    return referralList[investor];
+  }
+}
